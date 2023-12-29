9995c568792f5f59fcb52fcf248d1570f6efd34d
DazaiOsamu2111 <144069488+DazaiOsamu2111@users.noreply.github.com>
2023-12-29 18:39:03 +0300

    next

--- HTML.js
+++ HTML.js
@@ -1,10 +1,15 @@
 export class Button {
+	divMain
+	divButton
+	divIMG
+	divSignature
 	constructor(top) {
 		top = this.top
+		//this.divMain = document.createElement('div')
+		//this.divMain.style.cssText += `
+		//top: ${top};
+		//`
 		this.divMain = document.createElement('div')
-		this.divMain.style.cssText += `
-			top: ${top};
-		`
 		this.divButton = document.createElement('div')
 		this.divIMG = document.createElement('div')
 		this.divSignature = document.createElement('div')
