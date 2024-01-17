19d2c635aa46bbbfbb79f500c539a1ad3eb65541
DazaiOsamu2111 <144069488+DazaiOsamu2111@users.noreply.github.com>
2024-01-17 12:42:40 +0300

    stp

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+//import { createRequire } from 'module'
 import { Button } from './HTML'
+//const require = createRequire(import.meta.url)
 var ButtonTest = new Button()
 ButtonTest.settingDivMain('100px', '100px', 'absolute', '100px', '100px', '100')
 console.log(ButtonTest.getDivMain())
@@ -34,12 +36,111 @@ const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
-let fs = require('fs')
-const occtimportjs = require('occt-import-js')()
 
+async function LoadGeometry(targetObject) {
+	// init occt-import-js
+	const occt = await occtimportjs()
+
+	// download a step file
+	let fileUrl =
+		'https://raw.githubusercontent.com/kovacsv/occt-import-js/main/test/testfiles/cax-if/as1_pe_203.stp'
+	let response = await fetch(fileUrl)
+	let buffer = await response.arrayBuffer()
+
+	// read the imported step file
+	let fileBuffer = new Uint8Array(buffer)
+	let result = occt.ReadStepFile(fileBuffer, null)
+
+	// process the geometries of the result
+	for (let resultMesh of result.meshes) {
+		let geometry = new THREE.BufferGeometry()
+
+		geometry.setAttribute(
+			'position',
+			new THREE.Float32BufferAttribute(resultMesh.attributes.position.array, 3)
+		)
+		if (resultMesh.attributes.normal) {
+			geometry.setAttribute(
+				'normal',
+				new THREE.Float32BufferAttribute(resultMesh.attributes.normal.array, 3)
+			)
+		}
+		const index = Uint32Array.from(resultMesh.index.array)
+		geometry.setIndex(new THREE.BufferAttribute(index, 1))
+
+		let material = null
+		if (resultMesh.color) {
+			const color = new THREE.Color(
+				resultMesh.color[0],
+				resultMesh.color[1],
+				resultMesh.color[2]
+			)
+			material = new THREE.MeshPhongMaterial({
+				color: color,
+			})
+		} else {
+			material = new THREE.MeshPhongMaterial({
+				color: 0xcccccc,
+			})
+		}
+
+		const mesh = new THREE.Mesh(geometry, material)
+		targetObject.add(mesh)
+	}
+}
+
+async function Load() {
+	const width = window.innerWidth
+	const height = window.innerHeight
+
+	const renderer = new THREE.WebGLRenderer({
+		antialias: true,
+	})
+	renderer.setSize(width, height)
+	renderer.setClearColor(0xfafafa)
+	document.body.appendChild(renderer.domElement)
+
+	const camera = new THREE.PerspectiveCamera(45, width / height, 1.0, 100000.0)
+	camera.position.set(5000.0, 15000.0, 10000.0)
+	camera.up.set(0.0, 0.0, 1.0)
+	camera.lookAt(new THREE.Vector3(0.0, 0.0, 0.0))
+
+	const scene = new THREE.Scene()
+
+	const ambientLight = new THREE.AmbientLight(0x444444)
+	scene.add(ambientLight)
+
+	const directionalLight = new THREE.DirectionalLight(0x888888)
+	directionalLight.position.set(
+		camera.position.x,
+		camera.position.y,
+		camera.position.z
+	)
+	scene.add(directionalLight)
+
+	const mainObject = new THREE.Object3D()
+	LoadGeometry(mainObject)
+	scene.add(mainObject)
+
+	renderer.setAnimationLoop(time => {
+		mainObject.rotation.x = time / 2000
+		mainObject.rotation.y = time / 1000
+		renderer.render(scene, camera)
+	})
+}
+
+Load()
+
+/*
+//let fs = require('fs')
+import * as fs from 'fs'
+import * as occtimportjs from 'occt-import-js'
+//const occtimportjs = require('occt-import-js')()
+//import occtimportjs from 'node_modules/occt-import-js'
 occtimportjs.then(occt => {
-	let fileUrl = '../test/testfiles/simple-basic-cube/cube.stp'
+	let fileUrl = '/public/CLGR-D0019-HOLEFLATDRILL-STP.stp'
 	let fileContent = fs.readFileSync(fileUrl)
 	let result = occt.ReadStepFile(fileContent, null)
 	console.log(result)
 })
+*/
