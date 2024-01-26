81432f64e759c196d2503d6e3e6c0983bb557753
DazaiOsamu2111 <144069488+DazaiOsamu2111@users.noreply.github.com>
2024-01-26 09:04:56 +0300

    aaaa

--- main.js
+++ main.js
@@ -1,8 +1,523 @@
+var DivVVod = document.createElement('div')
+document.body.appendChild(DivVVod)
+DivVVod.style.position = 'absolute'
+DivVVod.style.width = '20%'
+DivVVod.style.height = '97%'
+DivVVod.style.zIndex = '100'
+DivVVod.style.top = '1%'
+DivVVod.style.right = '21%'
+DivVVod.style.background = '#afb2b6'
+DivVVod.style.border = '0'
+DivVVod.style.borderRadius = '5px'
+DivVVod.style.color = ' #000000'
+DivVVod.style.padding = '10px 16px'
+DivVVod.style.lineHeight = '200%'
+DivVVod.style.opacity = '0.8'
+var table = document.createElement('table')
+table.style.borderCollapse = 'collapse'
+table.style.border = ' 2px solid #000000'
+DivVVod.appendChild(table)
+let row = table.insertRow()
+row.style.borderCollapse = 'collapse'
+row.style.border = ' 2px solid #000000'
+let cell1 = row.insertCell()
+cell1.style.borderCollapse = 'collapse'
+cell1.style.border = ' 2px solid #000000'
+cell1.innerHTML = 'Наименование'
+let cell2 = row.insertCell()
+cell2.innerHTML = 'Описание'
+cell2.style.borderCollapse = 'collapse'
+cell2.style.border = ' 2px solid #000000'
+let cell3 = row.insertCell()
+cell3.innerHTML = 'Значение'
+cell3.style.borderCollapse = 'collapse'
+cell3.style.border = ' 2px solid #000000'
+//let cell4 = row.insertCell()
+//cell4.innerHTML = 'Позиция'
+//cell4.style.borderCollapse = 'collapse'
+//cell4.style.border = ' 2px solid #000000'
+
+var row1 = table.insertRow()
+row1.style.borderCollapse = 'collapse'
+row1.style.border = ' 2px solid #000000'
+var cell5 = row1.insertCell()
+cell5.style.borderCollapse = 'collapse'
+cell5.style.border = ' 2px solid #000000'
+cell5.innerHTML = 'Dc'
+var cell6 = row1.insertCell()
+cell6.innerHTML = 'Диаметр режущей кромки'
+cell6.style.borderCollapse = 'collapse'
+cell6.style.border = ' 2px solid #000000'
+var cell7 = row1.insertCell()
+cell7.innerHTML = '3мм'
+cell7.style.borderCollapse = 'collapse'
+cell7.style.border = ' 2px solid #000000'
+//var cell8 = row1.insertCell()
+//cell8.innerHTML = '1'
+//cell8.style.borderCollapse = 'collapse'
+//cell8.style.border = ' 2px solid #000000'
+
+var row1 = table.insertRow()
+row1.style.borderCollapse = 'collapse'
+row1.style.border = ' 2px solid #000000'
+var cell5 = row1.insertCell()
+cell5.style.borderCollapse = 'collapse'
+cell5.style.border = ' 2px solid #000000'
+cell5.innerHTML = 'Xs'
+var cell6 = row1.insertCell()
+cell6.innerHTML = 'Вылет'
+cell6.style.borderCollapse = 'collapse'
+cell6.style.border = ' 2px solid #000000'
+var cell7 = row1.insertCell()
+cell7.innerHTML = '39мм'
+cell7.style.borderCollapse = 'collapse'
+cell7.style.border = ' 2px solid #000000'
+//var cell8 = row1.insertCell()
+//cell8.innerHTML = '2'
+//cell8.style.borderCollapse = 'collapse'
+//cell8.style.border = ' 2px solid #000000'
+var row1 = table.insertRow()
+row1.style.borderCollapse = 'collapse'
+row1.style.border = ' 2px solid #000000'
+var cell5 = row1.insertCell()
+cell5.style.borderCollapse = 'collapse'
+cell5.style.border = ' 2px solid #000000'
+cell5.innerHTML = 'Ls'
+var cell6 = row1.insertCell()
+cell6.innerHTML = 'Длина режущей поверхности'
+cell6.style.borderCollapse = 'collapse'
+cell6.style.border = ' 2px solid #000000'
+var cell7 = row1.insertCell()
+cell7.innerHTML = '6.5мм'
+cell7.style.borderCollapse = 'collapse'
+cell7.style.border = ' 2px solid #000000'
+//var cell8 = row1.insertCell()
+//cell8.innerHTML = '3'
+//cell8.style.borderCollapse = 'collapse'
+//cell8.style.border = ' 2px solid #000000'
+var row1 = table.insertRow()
+row1.style.borderCollapse = 'collapse'
+row1.style.border = ' 2px solid #000000'
+var cell5 = row1.insertCell()
+cell5.style.borderCollapse = 'collapse'
+cell5.style.border = ' 2px solid #000000'
+cell5.innerHTML = 'L4'
+var cell6 = row1.insertCell()
+cell6.innerHTML = 'Максимальная глубина обработки'
+cell6.style.borderCollapse = 'collapse'
+cell6.style.border = ' 2px solid #000000'
+var cell7 = row1.insertCell()
+cell7.innerHTML = '12.5мм'
+cell7.style.borderCollapse = 'collapse'
+cell7.style.border = ' 2px solid #000000'
+//var cell8 = row1.insertCell()
+//cell8.innerHTML = '4'
+//cell8.style.borderCollapse = 'collapse'
+//cell8.style.border = ' 2px solid #000000'
+var row1 = table.insertRow()
+row1.style.borderCollapse = 'collapse'
+row1.style.border = ' 2px solid #000000'
+var cell5 = row1.insertCell()
+cell5.style.borderCollapse = 'collapse'
+cell5.style.border = ' 2px solid #000000'
+cell5.innerHTML = 'Ltot'
+var cell6 = row1.insertCell()
+cell6.innerHTML = 'Общая длина компонентов'
+cell6.style.borderCollapse = 'collapse'
+cell6.style.border = ' 2px solid #000000'
+var cell7 = row1.insertCell()
+cell7.innerHTML = '39мм'
+cell7.style.borderCollapse = 'collapse'
+cell7.style.border = ' 2px solid #000000'
+//var cell8 = row1.insertCell()
+//cell8.innerHTML = '5'
+//cell8.style.borderCollapse = 'collapse'
+//cell8.style.border = ' 2px solid #000000'
+
+var row1 = table.insertRow()
+row1.style.borderCollapse = 'collapse'
+row1.style.border = ' 2px solid #000000'
+var cell5 = row1.insertCell()
+cell5.style.borderCollapse = 'collapse'
+cell5.style.border = ' 2px solid #000000'
+cell5.innerHTML = 'Dc2'
+var cell6 = row1.insertCell()
+cell6.innerHTML = 'Диаметр режущей поверхности 2'
+cell6.style.borderCollapse = 'collapse'
+cell6.style.border = ' 2px solid #000000'
+var cell7 = row1.insertCell()
+cell7.innerHTML = 'мм'
+cell7.style.borderCollapse = 'collapse'
+cell7.style.border = ' 2px solid #000000'
+//var cell8 = row1.insertCell()
+//cell8.innerHTML = '6'
+//cell8.style.borderCollapse = 'collapse'
+//cell8.style.border = ' 2px solid #000000'
+var row1 = table.insertRow()
+row1.style.borderCollapse = 'collapse'
+row1.style.border = ' 2px solid #000000'
+var cell5 = row1.insertCell()
+cell5.style.borderCollapse = 'collapse'
+cell5.style.border = ' 2px solid #000000'
+cell5.innerHTML = 'Sigma'
+var cell6 = row1.insertCell()
+cell6.innerHTML = 'Угол вершины'
+cell6.style.borderCollapse = 'collapse'
+cell6.style.border = ' 2px solid #000000'
+var cell7 = row1.insertCell()
+cell7.innerHTML = '90°'
+cell7.style.borderCollapse = 'collapse'
+cell7.style.border = ' 2px solid #000000'
+//var cell8 = row1.insertCell()
+//cell8.innerHTML = '14'
+//cell8.style.borderCollapse = 'collapse'
+//cell8.style.border = ' 2px solid #000000'
+var row1 = table.insertRow()
+row1.style.borderCollapse = 'collapse'
+row1.style.border = ' 2px solid #000000'
+var cell5 = row1.insertCell()
+cell5.style.borderCollapse = 'collapse'
+cell5.style.border = ' 2px solid #000000'
+cell5.innerHTML = 'a'
+var cell6 = row1.insertCell()
+cell6.innerHTML = 'Длина пластины'
+cell6.style.borderCollapse = 'collapse'
+cell6.style.border = ' 2px solid #000000'
+var cell7 = row1.insertCell()
+cell7.innerHTML = '1.5мм'
+cell7.style.borderCollapse = 'collapse'
+cell7.style.border = ' 2px solid #000000'
+//var cell8 = row1.insertCell()
+//cell8.innerHTML = '17'
+//cell8.style.borderCollapse = 'collapse'
+//cell8.style.border = ' 2px solid #000000'
+var row1 = table.insertRow()
+row1.style.borderCollapse = 'collapse'
+row1.style.border = ' 2px solid #000000'
+var cell5 = row1.insertCell()
+cell5.style.borderCollapse = 'collapse'
+cell5.style.border = ' 2px solid #000000'
+cell5.innerHTML = 'Helix'
+var cell6 = row1.insertCell()
+cell6.innerHTML = 'Направление винтовой линии Л/Р/Н'
+cell6.style.borderCollapse = 'collapse'
+cell6.style.border = ' 2px solid #000000'
+var cell7 = row1.insertCell()
+cell7.innerHTML = '-'
+cell7.style.borderCollapse = 'collapse'
+cell7.style.border = ' 2px solid #000000'
+//var cell8 = row1.insertCell()
+//cell8.innerHTML = '18'
+//cell8.style.borderCollapse = 'collapse'
+//cell8.style.border = ' 2px solid #000000'
+var row1 = table.insertRow()
+row1.style.borderCollapse = 'collapse'
+row1.style.border = ' 2px solid #000000'
+var cell5 = row1.insertCell()
+cell5.style.borderCollapse = 'collapse'
+cell5.style.border = ' 2px solid #000000'
+cell5.innerHTML = 'Lambda'
+var cell6 = row1.insertCell()
+cell6.innerHTML = '30°'
+cell6.style.borderCollapse = 'collapse'
+cell6.style.border = ' 2px solid #000000'
+var cell7 = row1.insertCell()
+cell7.innerHTML = 'Угол подъёма'
+cell7.style.borderCollapse = 'collapse'
+cell7.style.border = ' 2px solid #000000'
+//var cell8 = row1.insertCell()
+//cell8.innerHTML = '19'
+//cell8.style.borderCollapse = 'collapse'
+//cell8.style.border = ' 2px solid #000000'
+var row1 = table.insertRow()
+row1.style.borderCollapse = 'collapse'
+row1.style.border = ' 2px solid #000000'
+var cell5 = row1.insertCell()
+cell5.style.borderCollapse = 'collapse'
+cell5.style.border = ' 2px solid #000000'
+cell5.innerHTML = 'CDL'
+var cell6 = row1.insertCell()
+cell6.innerHTML = 'Направление вращения налево?'
+cell6.style.borderCollapse = 'collapse'
+cell6.style.border = ' 2px solid #000000'
+var cell7 = row1.insertCell()
+cell7.innerHTML = '0'
+cell7.style.borderCollapse = 'collapse'
+cell7.style.border = ' 2px solid #000000'
+//var cell8 = row1.insertCell()
+//cell8.innerHTML = '20'
+//cell8.style.borderCollapse = 'collapse'
+//cell8.style.border = ' 2px solid #000000'
+var row1 = table.insertRow()
+row1.style.borderCollapse = 'collapse'
+row1.style.border = ' 2px solid #000000'
+var cell5 = row1.insertCell()
+cell5.style.borderCollapse = 'collapse'
+cell5.style.border = ' 2px solid #000000'
+cell5.innerHTML = 'Coolant'
+var cell6 = row1.insertCell()
+cell6.innerHTML = 'Внутренний подвод СОЖ'
+cell6.style.borderCollapse = 'collapse'
+cell6.style.border = ' 2px solid #000000'
+var cell7 = row1.insertCell()
+cell7.innerHTML = '0'
+cell7.style.borderCollapse = 'collapse'
+cell7.style.border = ' 2px solid #000000'
+//var cell8 = row1.insertCell()
+//cell8.innerHTML = '24'
+//cell8.style.borderCollapse = 'collapse'
+//cell8.style.border = ' 2px solid #000000'
+var row1 = table.insertRow()
+row1.style.borderCollapse = 'collapse'
+row1.style.border = ' 2px solid #000000'
+var cell5 = row1.insertCell()
+cell5.style.borderCollapse = 'collapse'
+cell5.style.border = ' 2px solid #000000'
+cell5.innerHTML = 'DHB'
+var cell6 = row1.insertCell()
+cell6.innerHTML = 'Диаметр корпуса'
+cell6.style.borderCollapse = 'collapse'
+cell6.style.border = ' 2px solid #000000'
+var cell7 = row1.insertCell()
+cell7.innerHTML = 'мм'
+cell7.style.borderCollapse = 'collapse'
+cell7.style.border = ' 2px solid #000000'
+//var cell8 = row1.insertCell()
+//cell8.innerHTML = '30'
+//cell8.style.borderCollapse = 'collapse'
+//cell8.style.border = ' 2px solid #000000'
+var row1 = table.insertRow()
+row1.style.borderCollapse = 'collapse'
+row1.style.border = ' 2px solid #000000'
+var cell5 = row1.insertCell()
+cell5.style.borderCollapse = 'collapse'
+cell5.style.border = ' 2px solid #000000'
+cell5.innerHTML = 'DS'
+var cell6 = row1.insertCell()
+cell6.innerHTML = 'Диаметр хвостовика'
+cell6.style.borderCollapse = 'collapse'
+cell6.style.border = ' 2px solid #000000'
+var cell7 = row1.insertCell()
+cell7.innerHTML = '3мм'
+cell7.style.borderCollapse = 'collapse'
+cell7.style.border = ' 2px solid #000000'
+//var cell8 = row1.insertCell()
+//cell8.innerHTML = '32'
+//cell8.style.borderCollapse = 'collapse'
+//cell8.style.border = ' 2px solid #000000'
+var row1 = table.insertRow()
+row1.style.borderCollapse = 'collapse'
+row1.style.border = ' 2px solid #000000'
+var cell5 = row1.insertCell()
+cell5.style.borderCollapse = 'collapse'
+cell5.style.border = ' 2px solid #000000'
+cell5.innerHTML = 'L Helix'
+var cell6 = row1.insertCell()
+cell6.innerHTML = 'Длина спирали'
+cell6.style.borderCollapse = 'collapse'
+cell6.style.border = ' 2px solid #000000'
+var cell7 = row1.insertCell()
+cell7.innerHTML = '11мм'
+cell7.style.borderCollapse = 'collapse'
+cell7.style.border = ' 2px solid #000000'
+//var cell8 = row1.insertCell()
+//cell8.innerHTML = '55'
+//cell8.style.borderCollapse = 'collapse'
+//cell8.style.border = ' 2px solid #000000'
+var DivVVod = document.createElement('div')
+document.body.appendChild(DivVVod)
+DivVVod.style.display = 'flex'
+DivVVod.style.justifyContent = 'center'
+var IMG = document.createElement('img')
+IMG.src = '/public/dxf2.png'
+DivVVod.style.position = 'absolute'
+//DivVVod.style.height = '375px'
+IMG.style.width = '51%'
+IMG.style.height = '51%'
+DivVVod.style.top = '5%'
+DivVVod.style.right = '-10%'
+DivVVod.appendChild(IMG)
+var DivVVod = document.createElement('div')
+DivVVod.style.display = 'flex'
+DivVVod.style.justifyContent = 'center'
+document.body.appendChild(DivVVod)
+var IMG = document.createElement('img')
+IMG.src = '/public/dxf.png'
+DivVVod.style.position = 'absolute'
+//DivVVod.style.height = '375px'
+IMG.style.width = '50%'
+IMG.style.height = '50%'
+DivVVod.style.top = '55%'
+DivVVod.style.right = '-10%'
+DivVVod.appendChild(IMG)
+var DivVVod = document.createElement('div')
+document.body.appendChild(DivVVod)
+DivVVod.style.position = 'absolute'
+DivVVod.style.width = '20%'
+DivVVod.style.height = '95%'
+DivVVod.style.zIndex = '100'
+DivVVod.style.top = '1%'
+DivVVod.style.left = '1%'
+DivVVod.style.background = '#afb2b6'
+DivVVod.style.border = '0'
+DivVVod.style.borderRadius = '5px'
+DivVVod.style.color = ' #000000'
+DivVVod.style.padding = '10px 16px'
+DivVVod.style.lineHeight = '200%'
+DivVVod.style.opacity = '0.8'
+
+//DivVVod.style.fontSize = '16px'
+//DivVVod.innerHTML = 'NEXT'
+//DivVVod.style.textAlign = 'center'
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = 'Основные данные'
+Zagolovok.style.fontWeight = 'bold'
+Zagolovok.style.textAlign = 'left'
+Zagolovok.style.fontSize = '25px'
+DivVVod.appendChild(Zagolovok)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = 'Идентификация инструментальной единицы'
+Zagolovok.style.fontWeight = 'bold'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = 'SSI004884'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = 'ID:0'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = 'Центровка 3.00мм Applitec'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = 'Чертеж:'
+Zagolovok.style.fontWeight = 'bold'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = 'SSI004884'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+console.log(DivVVod)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = 'Класс инструмента 01:'
+Zagolovok.style.fontWeight = 'bold'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = 'СВЕРЛО ЦЕНТРОВОЧНОЕ'
+//Zagolovok.style.fontWeight = 'bold'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+console.log(DivVVod)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = 'Группа инструмента 01'
+Zagolovok.style.fontWeight = 'bold'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = 'ЦЕНТР,СВЕРЛО NC'
+DivVVod.appendChild(Zagolovok)
+//Zagolovok.style.fontWeight = 'bold'
+Zagolovok.style.textAlign = 'left'
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = 'Масса'
+Zagolovok.style.fontWeight = 'bold'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = '0.004'
+//Zagolovok.style.fontWeight = 'bold'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = 'Ссылка на источник'
+Zagolovok.style.fontWeight = 'bold'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = 'A1174-3'
+//Zagolovok.style.fontWeight = 'bold'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = 'Тип использования'
+Zagolovok.style.fontWeight = 'bold'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = 'Расходуемый'
+//Zagolovok.style.fontWeight = 'bold'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = 'Режущий сплав VHM ALTSN'
+Zagolovok.style.fontWeight = 'bold'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = 'VHM AITiSN'
+//Zagolovok.style.fontWeight = 'bold'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = 'Количество режущих кромок'
+Zagolovok.style.fontWeight = 'bold'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = '2'
+//Zagolovok.style.fontWeight = 'bold'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = 'Система измерения'
+Zagolovok.style.fontWeight = 'bold'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = 'Метрическая'
+//Zagolovok.style.fontWeight = 'bold'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = 'Статус 1'
+Zagolovok.style.fontWeight = 'bold'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = '|01'
+//Zagolovok.style.fontWeight = 'bold'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = 'НОВЫЙ ИНСТРУМЕНТ'
+//Zagolovok.style.fontWeight = 'bold'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = 'Статус 2'
+Zagolovok.style.fontWeight = 'bold'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = '|01'
+//Zagolovok.style.fontWeight = 'bold'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
+var Zagolovok = document.createElement('div')
+Zagolovok.innerHTML = 'НОВЫЙ ИНСТРУМЕНТ'
+//Zagolovok.style.fontWeight = 'bold'
+Zagolovok.style.textAlign = 'left'
+DivVVod.appendChild(Zagolovok)
 //import { createRequire } from 'module'
 import { Button } from './HTML'
 //const require = createRequire(import.meta.url)
 var ButtonTest = new Button()
-ButtonTest.settingDivMain('100px', '100px', 'absolute', '100px', '100px', '100')
+//ButtonTest.settingDivMain('100px', '100px', 'absolute', '100px', '100px', '100')
 console.log(ButtonTest.getDivMain())
 //import
 import * as THREE from 'three'
@@ -43,7 +558,7 @@ async function LoadGeometry(targetObject) {
 	const occt = await occtimportjs()
 
 	// download a step file
-	let fileUrl = 'public/CLGR-D0019-HOLEFLATDRILL-STP.stp'
+	let fileUrl = 'public/SSI004884.stp'
 	let response = await fetch(fileUrl)
 	let buffer = await response.arrayBuffer()
 
